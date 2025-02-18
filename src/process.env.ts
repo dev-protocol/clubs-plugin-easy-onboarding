8bb2a7658ffeac98881975c9ce4fec6d009dbfaf
Aggre <hiroyuki.aggre@gmail.com>
2025-02-18 10:17:54 +0900

    fix

--- src/process.env.ts
+++ src/process.env.ts
@@ -1,6 +1,6 @@
 // @dynamic-labs/iconic, a package what @dynamic-labs/sdk-react-core is required, is requiring `process` on the frontend.
 // eslint-disable-next-line functional/no-conditional-statements
-if (typeof window.process === 'undefined') {
+if (typeof window !== 'undefined' && typeof window.process === 'undefined') {
 	// eslint-disable-next-line functional/no-expression-statements, functional/immutable-data, @typescript-eslint/no-explicit-any
 	;(window as any).process = { env: {} }
 }
